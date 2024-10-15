d348bd860dd7d49d4cf9f6b6caa9e118a9f2727b
gongsy1221 <gongsy0724@naver.com>
2024-10-15 22:07:12 +0900

    Sleep Customer

--- Cinema_Manager/Assets/02.Scripts/Customer/BT/Condition/UseTable.cs
+++ Cinema_Manager/Assets/02.Scripts/Customer/BT/Condition/UseTable.cs
@@ -6,13 +6,16 @@ public class UseTable : Conditional
     public SharedCustomer customer;
     public float eatTime; // 하나 먹는데 걸리는 시간
 
-    private float lastEatTime = 0;
-    private int currentEat;
+    private float startEatTime = 0;
+    private int currentEat; // 현재 먹은 음식 갯수
+
+    public float clearTime; // 진상 퇴치하는데 걸리는 시간
+    private float startTime;
 
     public override void OnStart()
     {
         customer.Value.AnimationCompo.SeatAnimation(1);
-        lastEatTime = Time.time;
+        startEatTime = Time.time;
         //currentEat = customer.Value.customerData.wantBuy;
     }
 
@@ -20,8 +23,21 @@ public class UseTable : Conditional
     {
         if (currentEat == 0)
         {
-            if(currentEat == 0 && customer.Value.CurrentCustomerType == CustomerType.Sleep)
+            if(customer.Value.CurrentCustomerType == CustomerType.Sleep)
+            {
+                customer.Value.AnimationCompo.SleepAnimation(1);
+
+                if (customer.Value.CheckPlayer())
+                {
+                    startTime = Time.time;
+                    if(clearTime <= Time.time - startTime)
+                    {
+                        customer.Value.AnimationCompo.SleepAnimation(-1);
+                        customer.Value.CurrentCustomerType = CustomerType.Basic;
+                    }
+                }
                 return TaskStatus.Running;
+            }
 
             customer.Value.currentChair.ChangeUsingState(false);
             customer.Value.currentChair.ChangeDirtyState(true);
@@ -30,10 +46,10 @@ public class UseTable : Conditional
             return TaskStatus.Failure;
         }
 
-        if (eatTime <= Time.time - lastEatTime)
+        if (eatTime <= Time.time - startEatTime)
         {
             currentEat--;
-            lastEatTime = Time.time;
+            startEatTime = Time.time;
         }
         return TaskStatus.Running;
     }
