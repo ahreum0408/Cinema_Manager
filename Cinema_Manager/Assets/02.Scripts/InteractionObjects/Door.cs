a92333a896ff32faf3002e9cc6042b5f384dcba4
Ayun14 <127943296+Ayun14@users.noreply.github.com>
2024-11-11 11:04:31 +0900

    Fix | 문 이상하던거 고침  Add | DisplayStand에 글씨 넣음

--- Cinema_Manager/Assets/02.Scripts/InteractionObjects/Door.cs
+++ Cinema_Manager/Assets/02.Scripts/InteractionObjects/Door.cs
@@ -10,6 +10,8 @@ public class Door : MonoBehaviour, IIneractionable
     private Quaternion _closedRotation;
     private Quaternion _openRotation;
 
+    private bool _isOpen = false;
+
     private Transform _visualTrm;
     private Coroutine _currentRotationRoutine;
 
@@ -28,19 +30,23 @@ public class Door : MonoBehaviour, IIneractionable
         float targetYAngle = dotProduct > 0 ? -_openAngle : _openAngle;
         _openRotation = Quaternion.Euler(0, targetYAngle, 0) * _closedRotation;
 
-        StartRotation(_openRotation);
+        StartRotation(_openRotation, true);
     }
 
     public void ExitInteraction(AgentController agent)
     {
-        StartRotation(_closedRotation);
+        StartRotation(_closedRotation, false);
     }
 
-    private void StartRotation(Quaternion targetRotation)
+    private void StartRotation(Quaternion targetRotation, bool isOpen)
     {
-        if (_currentRotationRoutine != null)
-            StopCoroutine(_currentRotationRoutine);
 
+        if (_currentRotationRoutine != null)
+        {
+            if (_isOpen && isOpen) return;
+            StopCoroutine(_currentRotationRoutine);
+        }
+        _isOpen = isOpen;
         _currentRotationRoutine = StartCoroutine(RotateDoorRoutine(targetRotation));
     }
 
