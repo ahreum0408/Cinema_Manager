c8c68834e191ad1eca53ba8af04233f78e7b3110
reum <ahreun0408@gmail.com>
2024-11-12 22:23:38 +0900

    add | levelup 했을 때 파티클 나오도록

--- Cinema_Manager/Assets/02.Scripts/UI/Events/LevelUpEvents.cs
+++ Cinema_Manager/Assets/02.Scripts/UI/Events/LevelUpEvents.cs
@@ -6,4 +6,5 @@ public static class LevelUpEvents {
     public static Action<GameData> GameDataLoadEvent;
 
     public static Action<GameData> LevelUpUpdate;
+    public static Action CloseView;
 }
