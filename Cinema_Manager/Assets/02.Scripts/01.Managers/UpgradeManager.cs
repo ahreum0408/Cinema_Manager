42e6f468e7af1e3259d851e6ce89700a69d78b6b
reum <ahreun0408@gmail.com>
2024-11-20 22:07:00 +0900

    add | 플레이어, 직원 업그레이드 할 때마다 돈 증가

--- Cinema_Manager/Assets/02.Scripts/01.Managers/UpgradeManager.cs
+++ Cinema_Manager/Assets/02.Scripts/01.Managers/UpgradeManager.cs
@@ -2,14 +2,30 @@ using System;
 using TMPro;
 using UnityEngine;
 
-public class UpgradeManager : MonoBehaviour {
+public enum UpgradeTarget {
+    playerMoveSpeedStat,
+    playerVolumeStat,
+    playerSellingcostStat,
+    employeeMoveSpeedStat, 
+    employeeVolumeVolumeStat,
+    employeeAddStat
+}
+public class UpgradeManager : MonoSingleton<UpgradeManager> {
+    [Header("Player")]
     public float[] playerMoveSpeedStat = new float[5];
     public int[] playerVolumeStat = new int[5];
     public float[] playerSellingcostStat = new float[5];
 
+    [Header("Player Upgrade Price")]
+    public int[] playerUpgradePrice = new int[5];
+
+    [Header("Empolyee")]
     public float[] employeeMoveSpeedStat = new float[5];
     public int[] employeeVolumeStat = new int[5];
     public int[] employeeAddStat = new int[5];
+
+    [Header("Empolyee Upgrade Price")]
+    public int[] employeeUpgradePrice = new int[5];
 
     private GameData _gameData;
 
@@ -60,12 +76,51 @@ public class UpgradeManager : MonoBehaviour {
 
         _player.SetPlayerStat(costWeight, speed, stack);
     }
-
     private void SettingEmployeeStat() {
         float speed = employeeMoveSpeedStat[_gameData.e_movespeedLevel];
         int stack = employeeVolumeStat[_gameData.e_volumeLevel];
         int employeeCount = employeeAddStat[_gameData.e_employmentLevel];
 
         StaffManager.Instance.SetStaffStat(speed, stack, employeeCount);
+    }
+    public void FindDataAndCalculate(UpgradeTarget target) {
+        int minusCoin = FindMatchData(target, 1);
+        CalcualteMinusCoin(minusCoin);
+    }
+    private void CalcualteMinusCoin(int coin) {
+        CoinManager.Instance.Coin -= coin;
+    }
+    public bool CanUpgrade(UpgradeTarget target) {
+        int minusCoin = FindMatchData(target);
+        if (CoinManager.Instance.Coin - minusCoin < 0) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    private int FindMatchData(UpgradeTarget target, int minusIndex = 0) {
+        int index = 0, minusCoin = 0;
+        switch (target) {
+            case UpgradeTarget.playerMoveSpeedStat:
+                minusCoin = playerUpgradePrice[_gameData.p_movespeedLevel - minusIndex];
+                break;
+            case UpgradeTarget.playerVolumeStat:
+                minusCoin = playerUpgradePrice[_gameData.p_volumeLevel - minusIndex];
+                break;
+            case UpgradeTarget.playerSellingcostStat:
+                minusCoin = playerUpgradePrice[_gameData.p_sellingcostLevel - minusIndex];
+                break;
+            case UpgradeTarget.employeeMoveSpeedStat:
+                minusCoin = employeeUpgradePrice[_gameData.e_movespeedLevel - minusIndex];
+                break;
+            case UpgradeTarget.employeeVolumeVolumeStat:
+                minusCoin = employeeUpgradePrice[_gameData.e_volumeLevel - minusIndex];
+                break;
+            case UpgradeTarget.employeeAddStat:
+                minusCoin = employeeUpgradePrice[_gameData.e_employmentLevel - minusIndex];
+                break;
+        }
+        return minusCoin;
     }
 }
