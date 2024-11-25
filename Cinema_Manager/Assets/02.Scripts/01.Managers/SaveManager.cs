90fe2b34b9f4efcc1de092363194ee3b44c13341
Ayun14 <127943296+Ayun14@users.noreply.github.com>
2024-11-25 16:12:34 +0900

    Change | 이상한 using 삭제

--- Cinema_Manager/Assets/02.Scripts/01.Managers/SaveManager.cs
+++ Cinema_Manager/Assets/02.Scripts/01.Managers/SaveManager.cs
@@ -1,73 +1,87 @@
 using System;
-using TMPro;
-using UnityEditor.TextCore.Text;
 using UnityEngine;
-using UnityEngine.Playables;
 
 
 [RequireComponent(typeof(GameDataManager))]
-public class SaveManager : MonoBehaviour {
+public class SaveManager : MonoBehaviour
+{
     public static event Action<GameData> GameDataLoadedEvent;
 
     [SerializeField] private string _saveFilename = "savegame.dat";
 
     private GameDataManager gameDataManager;
 
-    void Awake() {
+    void Awake()
+    {
         gameDataManager = GetComponent<GameDataManager>();
     }
-    public class QuitApplicationUtility {
-        public static void MoveAndroidApplicationToBack() {
+    public class QuitApplicationUtility
+    {
+        public static void MoveAndroidApplicationToBack()
+        {
             AndroidJavaObject activity = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
             activity.Call<bool>("moveTaskToBack", true);
         }
     }
-    void OnApplicationQuit() {
+    void OnApplicationQuit()
+    {
 
         SaveGameData();
 
     }
-    private void OnApplicationFocus(bool focus) {
-        if (!focus) {
+    private void OnApplicationFocus(bool focus)
+    {
+        if (!focus)
+        {
             SaveGameData();
         }
     }
-    private void OnApplicationPause(bool pause) {
-        if (pause) {
+    private void OnApplicationPause(bool pause)
+    {
+        if (pause)
+        {
             SaveGameData();
         }
     }
 
-    void OnEnable() {
+    void OnEnable()
+    {
         MainEvents.ShowViewEvent += ViewShown;
         MainEvents.UpdateViewEvent += ViewUpdated;
     }
 
-    void OnDisable() {
+    void OnDisable()
+    {
         MainEvents.ShowViewEvent -= ViewShown;
         MainEvents.UpdateViewEvent -= ViewUpdated;
     }
-    public GameData NewData() {
+    public GameData NewData()
+    {
         return new GameData();
     }
 
-    public void LoadGame() {
-        if (gameDataManager.GameData == null) {
+    public void LoadGame()
+    {
+        if (gameDataManager.GameData == null)
+        {
             gameDataManager.GameData = NewData();
         }
-        if (FileManager.LoadFromFile(_saveFilename, out var jsonString)) {
+        if (FileManager.LoadFromFile(_saveFilename, out var jsonString))
+        {
             gameDataManager.GameData.LoadJson(jsonString);
             if (gameDataManager.GameData.isMinimumExecution == false) // 아직 튜토리얼은 안했어
             {
                 gameDataManager.GameData = NewData();
             }
         }
-        if (gameDataManager.GameData != null) {
+        if (gameDataManager.GameData != null)
+        {
             GameDataLoadedEvent?.Invoke(gameDataManager.GameData);
         }
     }
 
-    public void SaveGameData() {
+    public void SaveGameData()
+    {
         string jsonFile = gameDataManager.GameData.ToJson();
         FileManager.WriteToFile(_saveFilename, jsonFile);
     }
